c24c682a3a9a42035c1a6a9621a0a25b827021d0
Benjamin Lupton <b@lupton.cc>
2016-05-02 06:51:50 +0800

    v1.1.0. CoffeeScript to ESNext.

--- source/index.js
+++ source/index.js
@@ -1,44 +1,50 @@
-# Import
-pathUtil = require('path')
-ignorePatterns = require('ignorepatterns')
+// Import
+const pathUtil = require('path')
+const ignorePatterns = require('ignorepatterns')
 
-# Define
-ignoreFS =
-	# Is Ignored Path
-	# opts={ignorePaths, ignoreHiddenFiles, ignoreCommonPatterns, ignoreCustomPatterns}
-	# returns true/false
-	isIgnoredPath: (path, opts={}) ->
-		# Prepare
-		result = false
-		basename = pathUtil.basename(path)
-		opts.ignorePaths ?= false
-		opts.ignoreHiddenFiles ?= false
-		opts.ignoreCommonPatterns ?= true
-		opts.ignoreCustomPatterns ?= false
+// Is Ignored Path
+// opts={ignorePaths, ignoreHiddenFiles, ignoreCommonPatterns, ignoreCustomPatterns}
+// returns true/false
+function isIgnoredPath (path, opts) {
+	// Prepare
+	let result = false
+	const basename = pathUtil.basename(path)
 
-		# Fetch the common patterns to ignore
-		if opts.ignoreCommonPatterns is true
-			opts.ignoreCommonPatterns = ignorePatterns
+	// Defaults
+	if ( !opts )  opts = {}
+	if ( opts.ignorePaths == null )  opts.ignorePaths = false
+	if ( opts.ignoreHiddenFiles == null )  opts.ignoreHiddenFiles = false
+	if ( opts.ignoreCommonPatterns == null )  opts.ignoreCommonPatterns = true
+	if ( opts.ignoreCustomPatterns == null )  opts.ignoreCustomPatterns = false
 
-		# Test Ignore Paths
-		if opts.ignorePaths
-			for ignorePath in opts.ignorePaths
-				if path.indexOf(ignorePath) is 0
-					result = true
-					break
+	// Fetch the common patterns to ignore
+	if ( opts.ignoreCommonPatterns === true ) {
+		opts.ignoreCommonPatterns = ignorePatterns
+	}
 
-		# Test Ignore Patterns
-		result =
-			result or
-			(opts.ignoreHiddenFiles    and /^\./.test(basename)) or
-			(opts.ignoreCommonPatterns and opts.ignoreCommonPatterns.test(basename)) or
-			(opts.ignoreCommonPatterns and opts.ignoreCommonPatterns.test(path)) or
-			(opts.ignoreCustomPatterns and opts.ignoreCustomPatterns.test(basename)) or
-			(opts.ignoreCustomPatterns and opts.ignoreCustomPatterns.test(path)) or
-			false
+	// Test Ignore Paths
+	if ( opts.ignorePaths ) {
+		for ( const ignorePath of opts.ignorePaths ) {
+			if ( path.indexOf(ignorePath) === 0 ) {
+				result = true
+				break
+			}
+		}
+	}
 
-		# Return
-		return result
+	// Test Ignore Patterns
+	result =
+		result ||
+		(opts.ignoreHiddenFiles    && (/^\./).test(basename)) ||
+		(opts.ignoreCommonPatterns && opts.ignoreCommonPatterns.test(basename)) ||
+		(opts.ignoreCommonPatterns && opts.ignoreCommonPatterns.test(path)) ||
+		(opts.ignoreCustomPatterns && opts.ignoreCustomPatterns.test(basename)) ||
+		(opts.ignoreCustomPatterns && opts.ignoreCustomPatterns.test(path)) ||
+		false
 
-# Export
-module.exports = ignoreFS
+	// Return
+	return result
+}
+
+// Export
+module.exports = {isIgnoredPath}
--- source/test.js
+++ source/test.js
@@ -1,12 +1,103 @@
-# Import
-{expect, assert} = require('chai')
-joe = require('joe')
-ignoreFS = require('../../')
+// Import
+const {equal} = require('assert-helpers')
+const joe = require('joe')
+const ignorefs = require('../')
 
+// Tests
+joe.suite('ignorefs', function (suite) {
 
-# =====================================
-# Tests
+	suite('common patterns', function (suite, test) {
+		// Prepare
+		const ignoreExpected = {
+			// Vim
+			'~': true,
+			'~something': true,
+			'something~': true,
+			'something~something': false,
 
-joe.describe 'ignorefs', (describe, it) ->
-	it 'should pass with no tests', ->
-		console.log(ignoreFS)
+			// Emacs
+			'.#': true,
+			'.#something': true,
+			'something.#': false,
+			'something.#something': false,
+
+			// Vi
+			'.swp': true,
+			'aswp': false,
+			'something.swp': true,
+			'.swpsomething': false,
+
+			// SVN
+			'.svn': true,
+			'asvn': false,
+			'something.svn': false,
+			'something.svnsomething': false,
+
+			// GIT
+			'.git': true,
+			'agit': false,
+			'something.git': false,
+			'something.gitsomething': false,
+
+			// HG
+			'.hg': true,
+			'ahg': false,
+			'something.hg': false,
+			'something.hgsomething': false,
+
+			// DS_Store
+			'.DS_Store': true,
+			'something.DS_Store': false,
+			'something.DS_Storesomething': false,
+
+			// Node
+			'node_modules': true,
+			'somethingnode_modules': false,
+			'somethingnode_modulessomething': false,
+
+			// CVS
+			'CVS': true,
+			'somethingCVS': false,
+			'somethingCVSsomething': false,
+
+			// Thumbs
+			'thumbs.db': true,
+			'thumbsadb': false,
+
+			// Desktop
+			'desktop.ini': true,
+			'desktopaini': false
+		}
+
+		// Tests
+		Object.keys(ignoreExpected).forEach(function (path) {
+			const resultExpected = ignoreExpected[path]
+			const testName = `${resultExpected ? 'should' : 'should not'} ignore ${path}`
+			test(testName, function () {
+				const resultActual = ignorefs.isIgnoredPath(path)
+				equal(resultActual, resultExpected, 'ignored result was as expected')
+			})
+		})
+	})
+
+	suite('ignore paths', function (suite, test) {
+		// Prepare
+		const ignoreExpected = {
+			'/usr/awesome': false,
+			'/tmp/awesome': true,
+			'/usr/tmp/awesome': true
+		}
+
+		// Tests
+		Object.keys(ignoreExpected).forEach(function (path) {
+			const resultExpected = ignoreExpected[path]
+			const testName = `${resultExpected ? 'should' : 'should not'} ignore ${path}`
+			test(testName, function () {
+				const resultActual = ignorefs.isIgnoredPath(path, {
+					ignorePaths: ['/tmp', '/usr/tmp']
+				})
+				equal(resultActual, resultExpected, 'ignored result was as expected')
+			})
+		})
+	})
+})
